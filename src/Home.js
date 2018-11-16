09f6d924d8db16f929c59d4f6ec3218d98dc2c45
OnurSevket <onursevket44@gmail.com>
2018-11-16 13:20:06 +0300

    SPA using react-router

---
+++ src/Home.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h2>Hello</h2>
+                <p>
+                Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit. 
+                Duis dictum tempor neque. Nullam molestie,
+                 quam id dignissim lacinia, nisi massa tempor libero, 
+                 ut finibus ex nisl ac urna. Nam pulvinar efficitur justo. 
+                 Pellentesque gravida vulputate varius. Donec justo mauris, 
+                 sodales id sagittis pulvinar, molestie eget felis. 
+                 Curabitur euismod tempus metus. Quisque mollis dapibus cursus.
+                  Sed accumsan sapien nec nibh mollis maximus
+                </p>
+                <p>
+                Sed accumsan sapien nec nibh mollis maximus
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Home;
